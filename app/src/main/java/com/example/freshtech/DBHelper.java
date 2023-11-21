6b1a779e29116ed949d9c247c38b3fbbbae202d9
Jason Bekker <jasonbekker45@gmail.com>
2023-11-21 14:35:38 +0300

    second commit

---
+++ app/src/main/java/com/example/freshtech/DBHelper.java
@@ -0,0 +1,27 @@
+package com.example.freshtech;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper  extends SQLiteOpenHelper {
+    public static final String DBNAME = "App.db";
+
+    final String secretKey="donottouch";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DBNAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
--- app/src/main/java/com/example/freshtech/SignIn.java
+++ app/src/main/java/com/example/freshtech/SignIn.java
@@ -2,13 +2,36 @@ package com.example.freshtech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
+    TextView btnsignup1;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btnsignup1 = (TextView) findViewById(R.id.btnsignup1);
+
+        btnsignup1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(SignIn.this,SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
 }
