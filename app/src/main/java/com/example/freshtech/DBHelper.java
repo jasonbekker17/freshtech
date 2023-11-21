3ef67645276af3e88f13bd33800b3624290ee5bb
Jason Bekker <jasonbekker45@gmail.com>
2023-11-21 15:24:17 +0300

    commit

--- app/src/main/java/com/example/freshtech/DBHelper.java
+++ app/src/main/java/com/example/freshtech/DBHelper.java
@@ -1,5 +1,6 @@
 package com.example.freshtech;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -16,12 +17,29 @@ public class DBHelper  extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create Table users(id INTEGER PRIMARY KEY AUTOINCREMENT, fullname TEXT, email TEXT, password VARCHAR)");
+
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop Table if exists users");
+    }
+
+    public boolean insertData(String email, String fullname, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("fullname", fullname);
+        cv.put("email", email);
+        cv.put("password", password);
+
+        long result = db.insert("users", null, cv);
+        if(result == -1) return false;
+        else
+            return true;
 
     }
 }
+
